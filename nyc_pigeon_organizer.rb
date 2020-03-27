6339b2845628abf86bd67fe52a32b435d28f0b91
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-27 02:41:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,17 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  binding.pry
-  
-  data.each do |sort, (key, value)|
-    data[:gender][:male]
+  i = 0 
+  while i < data.length do 
     
+  
   end 
   
   
 end
+
+  
+  #data.each do |sort, (key, value)|
+  #  data[:gender][:male]
+    
+  #end 
