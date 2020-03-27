0ae6cb0c142acd9fcfd0ec74820b427f412ce954
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-27 04:01:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,10 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         new_hash[name] = {} if !new_hash[name]
         if new_hash[name][attribute]
-          new_hash[name][attribute] = [inner_key]  
+          new_hash[name][attribute].push(inner_key.to_s)
+        else 
+          new_hash[name][attribute] = [inner_key.to_s]      
+        end 
       end 
     end 
   end 
